5d63e4d67b21917c064af72144e049db47571f24
ShevoFirst <play.job@yandex.ru>
2022-10-17 16:53:50 +0400

    первый коммит

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        //задача 1
+        var dog = 8;
+        var cat = 3.6f;
+        var paper = 763789;
+        System.out.println(dog+" "+cat+" "+ paper);
+        //задача 2
+        dog += 4;
+        cat += 4;
+        paper += 4;
+        System.out.println(dog+" "+cat+" "+ paper);
+        //задача 3
+        dog-=(int)3.5;
+        cat -=1.6;
+        paper -= 7639;
+        System.out.println(dog+" "+cat+" "+ paper);    }
+}
